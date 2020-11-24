f2898037c0bc8a2191b0ae3593312c3e5ec65082
Rabi Rao <raorabii88@gmail.com>
2020-11-24 23:56:01 +0300

    Updated Code for Homepage

---
+++ app/assets/javascripts/apps.js
@@ -0,0 +1,16 @@
+  
+  
+ 
+  
+
+
+ // var swiper = new Swiper('.swiper-container', {
+	// //loop: true,
+	// slidesPerView: 4,
+ //    spaceBetween: 30,
+ //    //autoplay:true,
+	// //autoplayTimeout:1000,
+ // });
+
+ 
+ 
